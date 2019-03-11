ddb83d3ad3e193eb37aea9a2719afbe4af9963d3
Soi <ycrxun@163.com>
2019-03-11 13:42:03 +0800

    :smile: image compress

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+find ./static -name '*.jpg' | xargs jpegoptim --strip-all
+find ./static -type f -name "*.png" -exec optipng {} \;
+
 hugo -b https://ronggle.com/
 
 git subtree add -P public https://github.com/ycrxun/ycrxun.github.io.git master
