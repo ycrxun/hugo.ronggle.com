4644c7548854a2fad4a7affe0829d3da8c128303
Soi <ycrxun@163.com>
2019-03-11 13:52:22 +0800

    :simle: add more

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
+git submodule update --init --recursive
+
 find ./static -name '*.jpg' | xargs jpegoptim --strip-all
 find ./static -type f -name "*.png" -exec optipng {} \;
 
 hugo -b https://ronggle.com/
+
+cd public/
+
+git add .
+git commit -m ":smile: new post"
+
+git push
+
+cd ..
+
+git add .
+git commit -m ":smile: new post"
+
+git push
